a2692cf2c2f5058705e9e2288b21b9010f981b27
SP-dvCfleck <61189189+SP-dvCfleck@users.noreply.github.com>
2020-02-25 00:37:28 +0000

    Setup toolbar, changed colors, created noticeboard fragment file

---
+++ app/src/main/java/sda/oscail/edu/giggidee/Noticeboard.java
@@ -0,0 +1,32 @@
+package sda.oscail.edu.giggidee;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Noticeboard extends Fragment {
+
+
+    public Noticeboard() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_noticeboard, container, false);
+        return root;
+    }
+
+}
